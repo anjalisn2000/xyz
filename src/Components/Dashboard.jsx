f6882a6e170805bb5f9e5b5e55851cee1175801b
16gsanket <16gsanket@gmail.com>
2024-08-16 23:34:50 +0530

    added documnts to the hoe page

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -4,27 +4,31 @@ const studentInfo1={
   name:"Daania qureshi",
   grade:"Engg",
   school:"VIT ",
-  scholarshipEligible:"false"
+  scholarshipEligible:"false",
+  documents:[{name:"pancard" , link: "https://adarshc.com/data/user/index/ent/document/pan/banner.jpg"}]
 }
 
 const studentInfo2 = {
   name:"Iqra Sheikh Bilal Abbas",
   grade:"Medical",
   school:'IIIT',
-  scholarshipEligible:"false"
+  scholarshipEligible:"false",
+  documents:[{name:"aadhar" , link:"https://staticimg.amarujala.com/assets/images/2022/12/08/kanapara_1670520411.jpeg?w=674&dpr=1.0"}]
 
 }
 const studentInfo3 = {
   name:"أنيرودا جوشي",
   grade:"طبي",
   school:'المعهد السعودي للطب',
-  scholarshipEligible:"true"
+  scholarshipEligible:"true",
+  documents:[{name:'aadhar' , link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlgvDZG_FnS9_8wWIUv5vPZgMPsE2_QSRUhg&s"}]
 }
 const studentInfo4 = {
   name:"Shanna Ahmed",
   grade:"Architecture",
   school:'المعهد السعودي للهندسة',
-  scholarshipEligible:"false"
+  scholarshipEligible:"false",
+  documents:[{name:'pancard' , link:"https://yourspj.wordpress.com/wp-content/uploads/2011/06/fake-pan-card_yourspj.jpg"}]
 }
 
 const Dashboard = () => {
@@ -65,6 +69,16 @@ const Dashboard = () => {
           <p>Grade: {studentInfo.grade}</p>
           <p>School/College: {studentInfo.school}</p>
           <p>Scholarship Eligibility: {studentInfo.scholarshipEligible}</p>
+          <h1 className='my-5 font-bold'>Uploaded Documents</h1>
+        <div className='h-[400px] w-full pt-10 flex'>
+            <div className='h-full w-[30%]'>
+
+              <p className='text-2xl font-bold'>{studentInfo.documents[0].name}</p>
+            </div>
+            <div className="h-[full] w-[70%]">
+              <img src={studentInfo.documents[0].link} alt=""  style={{height:"100%" , width:"100%", objectFit:"contain"}}/>
+            </div>
+        </div>
         </div>
       </main>
 
